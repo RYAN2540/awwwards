8ccab4e4447aaaafe1480676b29335096c7153c0
Your Name <you@example.com>
2021-04-06 09:06:21 +0300

    add profile and project methods and it's tests

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ serve:
 	python manage.py runserver
 
 migrations:
-	python manage.py makemigrations clone
+	python manage.py makemigrations awardsapp
 
 migrate:
 	python3.9 manage.py migrate
--- awardsapp/models.py
+++ awardsapp/models.py
@@ -6,6 +6,59 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     profile_pic = CloudinaryField('image')
     bio =  models.TextField()
-    location = models.CharField()
+    location = models.CharField(max_length = 60)
     email = models.EmailField()
     portfolio_link = models.URLField()
+
+    def __str__(self):
+        return self.user.username
+        
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete() 
+
+    def edit_bio(self, new_bio):
+        self.bio = new_bio
+        self.save()
+
+class Project(models.Model):
+    name = models.CharField(max_length = 30)
+    screenshot = CloudinaryField('image')
+    description = models.TextField()
+    link = models.URLField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True, null = True)
+    voters = models.ManyToManyField(Profile, related_name="votes")
+    design_score = models.IntegerField(default=0)
+    usability_score = models.IntegerField(default=0)
+    content_score = models.IntegerField(default=0)
+    average_design = models.FloatField(default=0,)
+    average_usability = models.FloatField(default=0)
+    average_content = models.FloatField(default=0)
+    average_score = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def voters_count(self):
+        return self.voters.count()
+
+    @classmethod
+    def display_all_projects(cls):
+        return cls.objects.all()
+
+    @classmethod 
+    def search_project(cls,name):
+        return Project.objects.filter(name__icontains = name)
+
+    @classmethod
+    def get_user_projects(cls,profile):
+        return cls.objects.filter(profile=profile)
