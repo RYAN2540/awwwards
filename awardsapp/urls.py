024c17ca55c9d66bc80459cf19d33f9a99789571
Your Name <you@example.com>
2021-04-06 08:49:39 +0300

    add profile model

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name = 'home'),
+    path('profile/',views.profile,name = 'profile'),
+    path('project/',views.project,name = 'project'),
 ]
--- awardsapp/views.py
+++ awardsapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def home(request):
     date = dt.date.today()
     return render(request, "user/profile.html", {"date": date})
+
+def profile(request):
+    return render(request, "user/profile.html")
+
+def project(request):
+    return render(request, "project/project.html")
