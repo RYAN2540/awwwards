8c2eed87fd62fd79b654582d8fd5de23db7aa2c4
Your Name <you@example.com>
2021-04-07 15:48:59 +0300

    display project details on homepage

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -6,9 +6,8 @@ urlpatterns=[
     path('email/',views.email,name = 'email'),
     path('create_profile/',views.create_profile,name = 'create_profile'),
     path('profile/',views.profile,name = 'profile'),
-    path('project/',views.project,name = 'project'),
+    path('project/(?P<project_id>\d+)',views.project,name = 'project'),
     path('add_project/',views.add_project,name = 'add_project'),
     path('rate_project/(?P<project_id>\d+)',views.rate_project,name = 'rate_project'),
     path('search_project/',views.search_project,name = 'search_project'),
-
 ]
