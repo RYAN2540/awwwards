d280910574e10d0ad76048ad4ac18f6f3d76d473
Your Name <you@example.com>
2021-04-07 09:52:44 +0300

    add search form in navbar

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     path('',views.home,name = 'home'),
     path('profile/',views.profile,name = 'profile'),
     path('project/',views.project,name = 'project'),
-    path('add-project/',views.add_project,name = 'add_project'),
-    path('rate-project/(?P<image_id>\d+)',views.rate_project,name = 'rate_project'),
+    path('add_project/',views.add_project,name = 'add_project'),
+    path('rate_project/(?P<image_id>\d+)',views.rate_project,name = 'rate_project'),
+    path('search_project/(?P<image_id>\d+)',views.search_project,name = 'search_project'),
 ]
