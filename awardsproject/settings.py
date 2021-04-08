a2cc7f96bcd29bccc3e47e2e157afd8682dc129b
Your Name <you@example.com>
2021-04-08 10:40:45 +0300

    deploy to heroku

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -46,9 +46,9 @@ INSTALLED_APPS = [
 ]
 
 cloudinary.config(
-  cloud_name = config('CLOUD_NAME'),
-  api_key = config('API_KEY'),
-  api_secret = config('API_SECRET'),
+  cloud_name = "dxrxqqdoc",
+  api_key = "649414433849725",
+  api_secret = "bU_lyM4us1cWy3zKp3sED7ievAY",
 )
 
 MIDDLEWARE = [
@@ -164,11 +164,11 @@ LOGOUT_REDIRECT_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ryandev8121@gmail.com'
+EMAIL_HOST_PASSWORD = 'crossfire254'
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
