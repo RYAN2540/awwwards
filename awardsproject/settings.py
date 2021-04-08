bc9c86c305f63704c981532312e670648ca823f1
Your Name <you@example.com>
2021-04-08 11:00:24 +0300

    deploy

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awardsapp',
-    'bootstrap4',
+    'bootstrap3',
     'cloudinary',
     'rest_framework',
 ]
